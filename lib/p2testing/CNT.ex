674bdad1e0445b7ec33f7d0bfea3d8d3b72ef9ff
Redvers Davies <red@infect.me>
2018-10-10 17:56:22 -0400

    Removed debugging statements

--- lib/p2testing/CNT.ex
+++ lib/p2testing/CNT.ex
@@ -10,33 +10,27 @@ defmodule P2Testing.CNT do
   end
 
   def handle_event(:info, :tick, :started, %{cnt: cnt, tref: tref, delay: delay}) do
-    IO.puts("tick: #{cnt}")
     timeref = :erlang.send_after(delay, self, :tick)
     {:next_state, :started, %{cnt: cnt+1, tref: timeref, delay: delay}}
   end
   def handle_event(:info, :tick, :stopped, %{cnt: cnt, tref: tref, delay: delay}) do
-    IO.puts("tick ignored")
     {:next_state, :stopped, %{cnt: cnt, tref: tref, delay: delay}}
   end
 
 
 
   def handle_event({:call, from}, :startCNT, :stopped, %{cnt: cnt, tref: tref, delay: delay}) do
-    IO.puts(":stopped -> :started")
     timeref = :erlang.send_after(delay, self, :tick)
     {:next_state, :started, %{cnt: cnt, tref: timeref, delay: delay}, [{:reply, from, %{cnt: cnt, tref: tref, delay: delay}}]}
   end
   def handle_event({:call, from}, :startCNT, :started, %{cnt: cnt, tref: tref, delay: delay}) do
-    IO.puts(":started -> :started (redundant)")
     {:next_state, :started, %{cnt: cnt, tref: tref, delay: delay}, [{:reply, from, %{cnt: cnt, tref: tref, delay: delay}}]}
   end
 
   def handle_event({:call, from}, :stopCNT, :started, %{cnt: cnt, tref: tref, delay: delay}) do
-    IO.puts(":started -> :stopped")
     {:next_state, :stopped, %{cnt: cnt, tref: tref, delay: delay}, [{:reply, from, %{cnt: cnt, tref: tref, delay: delay}}]}
   end
   def handle_event({:call, from}, :stopCNT, :stopped, %{cnt: cnt, tref: tref, delay: delay}) do
-    IO.puts(":stopped -> :stopped (redundant)")
     {:next_state, :stopped, %{cnt: cnt, tref: tref, delay: delay}, [{:reply, from, %{cnt: cnt, tref: tref, delay: delay}}]}
   end
 
