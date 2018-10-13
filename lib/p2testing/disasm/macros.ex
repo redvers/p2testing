156b8777cca6513f5f32c02bb38787ee58c8f05b
Redvers Davies <red@infect.me>
2018-10-13 09:57:59 -0400

    Disabling macros, checkpoint for mix format

--- lib/p2testing/disasm/macros.ex
+++ lib/p2testing/disasm/macros.ex
@@ -1,5 +1,5 @@
 defmodule P2Testing.Disasm.Macros do
   def t7czids({opcode,cc,zz,ii,dd,ss}) do
-   unquote(def disasm_mac(<<opcode :: size(7), cc :: size(1), zz :: size(1), ii :: size(1), dd :: size(9), ss :: size(9)>>), do: :ok)
+   unquote(def disasm_mac28(<<opcode :: size(7), cc :: size(1), zz :: size(1), ii :: size(1), dd :: size(9), ss :: size(9)>>), do: :ok)
   end
 end
